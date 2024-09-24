a12bab9dcd10585c1cdcd736bb3fec1776e192a6
rmoon187 <rabiul187@outlook.com>
2024-09-24 18:47:34 +0600

    operation part done

--- JS/main.js
+++ JS/main.js
@@ -8,23 +8,43 @@ window.addEventListener('scroll', () => {
   }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const blog = document.getElementById('btn-blog');
   const home = document.getElementById('btn-home');
-  
+
   if (blog) {
-      blog.addEventListener('click', function() {
-          window.location.href = 'blog.html';
-      });
+    blog.addEventListener('click', function () {
+      window.location.href = 'blog.html';
+    });
   }
 
   if (home) {
-      home.addEventListener('click', function() {
-          window.location.href = 'home.html';
-      });
+    home.addEventListener('click', function () {
+      window.location.href = 'home.html';
+    });
   }
 });
 
+// donation
+const mainBalanceElement = document.getElementById('balance');
+let mainBalance = parseFloat(mainBalanceElement.innerText); 
+const donateButtons = document.getElementsByClassName('donate-now');
 
+for (let i = 0; i < donateButtons.length; i++) {
+  donateButtons[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    const input = document.getElementsByClassName('input')[i];
+    let inputValue = parseFloat(input.value);
+    const cardElement = document.getElementsByClassName('card-value')[i];
+    let cardBalance = parseFloat(cardElement.innerText);
 
+    mainBalance-=inputValue;
+    cardBalance+=inputValue;
+
+    mainBalanceElement.innerText= mainBalance;
+    cardElement.innerText = cardBalance; 
+   
+    input.value = "";
+  });
+}
 
--- JS/utilities.js
+++ JS/utilities.js
@@ -9,13 +9,13 @@ function toggleButtons(activeBtnId) {
     const btnHistory = document.getElementById('btn-history');
 
     btnDonate.classList.remove('bg-[rgb(180,244,97)]', 'hover:bg-[rgb(158,214,85)]');
-    btnDonate.classList.add('bg-white');
+    btnDonate.classList.add('bg-white','border', 'border-[rgba(17,17,17,0.3)]','text-[rgba(17,17,17,0.7)]');
 
     btnHistory.classList.remove('bg-[rgb(180,244,97)]', 'hover:bg-[rgb(158,214,85)]');
-    btnHistory.classList.add('bg-white');
+    btnHistory.classList.add('bg-white','border', 'border-[rgba(17,17,17,0.3)]','text-[rgba(17,17,17,0.7)]');
 
     const activeBtn = document.getElementById(activeBtnId);
-    activeBtn.classList.remove('bg-white');
+    activeBtn.classList.remove('bg-white','border', 'border-[rgba(17,17,17,0.3)]','text-[rgba(17,17,17,0.7)]');
     activeBtn.classList.add('bg-[rgb(180,244,97)]', 'hover:bg-[rgb(158,214,85)]');
   }
 
