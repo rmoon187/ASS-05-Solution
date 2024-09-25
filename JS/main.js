f145734bc997b32ebc0ebdd1e388130b2516770e
rmoon187 <rabiul187@outlook.com>
2024-09-25 12:06:47 +0600

    some polishing done

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,19 @@
 
+// sticky header
 const stickySection = document.getElementById('stickySection');
+const navbar = document.querySelector('.navbar')
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
-    stickySection.classList.add('bg-white', 'bg-opacity-80', 'shadow-lg');
+    stickySection.classList.add('shadow-lg','backdrop-blur-lg', 'bg-white/20','z-50');
+    navbar.classList.add('backdrop-blur-lg','z-50','bg-white/20');
+    
   } else {
-    stickySection.classList.remove('bg-white', 'bg-opacity-80', 'shadow-lg');
+    stickySection.classList.remove( 'shadow-lg');
+    navbar.classList.remove('backdrop-blur-lg','bg-white/20');
   }
 });
 
+// navigate blog page
 document.addEventListener('DOMContentLoaded', function () {
   const blog = document.getElementById('btn-blog');
   const home = document.getElementById('btn-home');
@@ -52,7 +58,7 @@ for (let i = 0; i < donateButtons.length; i++) {
       // history
       const historySec = document.getElementById('history');
       const currentDate = new Date();
-      const formattedDate = currentDate.toLocaleString();
+      const formattedDate = currentDate.toString();
 
       const div = document.createElement('div');
       div.classList.add('max-w-screen-xl', 'mx-auto', 'mt-5');
@@ -60,11 +66,12 @@ for (let i = 0; i < donateButtons.length; i++) {
         <div class="card bg-base-100 border border-[rgba(17,17,17,0.3)] px-3 py-5 mb-5">
           <div class="card-body">
             <h2 class="card-title">${inputValue} Taka is donated for ${cardTitle}</h2>
-            <p>${formattedDate}</p>   
+            <p>Date: ${formattedDate}</p>   
           </div>
         </div>    
       `
       historySec.appendChild(div);
+
       // modal
       document.getElementById('my_modal_5').showModal();
       document.getElementById('modal-close-btn').addEventListener('click', function () {
@@ -72,7 +79,8 @@ for (let i = 0; i < donateButtons.length; i++) {
         input.value = "";
       }
     else {
-          alert('Invalid Output');
+          alert('Invalid Input');
+          input.value = "";
         }
     
   });
